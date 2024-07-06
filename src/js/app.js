92c1f82759738900c6c0b3f1c06808717cf4ae26
amirhosein-ardani <amirhosein-ardani@gmail.com>
2024-07-06 15:44:18 +0330

    add foreach in js

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,32 @@
-let form = document.getElementById('information-form');
-let users=[];
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    const userBook={
-        nameBook:e.target.book.value,
-        theWriter:e.target.writer.value,
-        genre:e.target.genre.value,
-        Price:e.target.Price.value,
+let form = document.getElementById("information-form");
+let users = [];
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const userBook = {
+    nameBook: e.target.book.value,
+    theWriter: e.target.writer.value,
+    genre: e.target.genre.value,
+    Price: e.target.Price.value,
+  };
 
-    };
-    
-    users.push(userBook);
-    console.log(users);
-})
+  users.push(userBook);
+  console.log(users);
+  updateTable();
+});
+function updateTable() {
+  const tableBody = document.getElementById("table-body");
+  tableBody.innerHTML = "";
+
+  users.forEach((userBook) => {
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+            <td>${userBook.nameBook}</td>
+            <td>${userBook.theWriter}</td>
+            <td>${userBook.genre}</td>
+            <td>${userBook.Price}</td>
+        `;
+
+    tableBody.appendChild(row);
+  });
+}
